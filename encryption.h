b635c16193cf6d556f3d9d962fa60989fe04fd06
Sarmad Al Kinany <draskinany@gmail.com>
2024-10-02 23:21:24 +0200

    Add function declarations for file encryption and decryption

---
+++ encryption.h
@@ -0,0 +1,9 @@
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
+
+#include <string>
+
+void encryptFile(const std::string& inputFile, const std::string& outputFile);
+void decryptFile(const std::string& inputFile, const std::string& outputFile);
+
+#endif // ENCRYPTION_H
