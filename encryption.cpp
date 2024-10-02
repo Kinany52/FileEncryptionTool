b50cf8981f910d2cf59c38f542a327108dce0725
Sarmad Al Kinany <draskinany@gmail.com>
2024-10-02 23:25:59 +0200

    Implement encryptFile and decryptFile functions

---
+++ encryption.cpp
@@ -0,0 +1,10 @@
+#include "encryption.h"
+#include <iostream>
+
+void encryptFile(const std::string& inputFile, const std::string& outputFile) {
+    std::cout << "Encrypting " << inputFile << " to " << outputFile << std::endl;
+}
+
+void decryptFile(const std::string& inputFile, const std::string& outputFile) {
+    std::cout << "Decrypting " << inputFile << " to " << outputFile << std::endl;
+}
